2cacd391954d30db0b69276bf4db5a86a35c1b1d
admin <admin@admin>
2024-04-01 22:12:50 +0800

    initial commit

---
+++ students/cite3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cite Faculties</title>
+    <style>
+        img {
+            
+        }
+    </style>
+</head>
+<body>
+
+    <!-- Replace "images/cite.jpg" with the path to your image -->
+    <img src="images/cite.jpg" style="max-width: 100%;height: auto;">
+
+    <!-- Back button -->
+    <a href="dashboard.php">Back</a>
+
+</body>
+</html>
